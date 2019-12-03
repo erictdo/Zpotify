a2b6ac7f1242df70e550c0cd8ce3577d607b8df2
seandinh97 <seandinhs@gmail.com>
2019-12-03 11:29:58 -0800

    Updated DFSCommand to include pull and push

--- src/server/src/main/java/dfs/DFSCommand.java
+++ src/server/src/main/java/dfs/DFSCommand.java
@@ -156,6 +156,10 @@ public class DFSCommand {
                 }
             } else if (result[0].equals("leave")) {
                 dfs.leave();
+            } else if (result[0].equals("push")) {
+
+            } else if (result[0].equals("pull")) {
+
             }
 
             dfsMenu();
@@ -199,6 +203,8 @@ public class DFSCommand {
         System.out.println(" append");
         System.out.println(" duplicate");
         System.out.println(" move");
+        System.out.println(" push");
+        System.out.println(" pull");
         System.out.println(" quit (to quit DFS Command and start server)");
         System.out.print("Enter a command: ");
 
