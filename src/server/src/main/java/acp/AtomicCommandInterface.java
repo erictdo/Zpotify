4558547ddf7a93cefc8132523ad85ce6db56ee8c
Eric Do <ericdo62497@gmail.com>
2019-12-02 20:34:37 -0800

    Added AtomicCommitInterface, Removed IDFSInterface

---
+++ src/server/src/main/java/acp/AtomicCommandInterface.java
@@ -0,0 +1,14 @@
+package main.java.acp;
+
+import main.java.dfs.Chord;
+
+import java.io.FileNotFoundException;
+import java.rmi.RemoteException;
+
+public interface AtomicCommandInterface {
+    public boolean canCommit(Transaction t, int guid) throws RemoteException, FileNotFoundException;
+    public void doCommit(Transaction t) throws RemoteException;
+    public void doAbort(Transaction t,int guid) throws RemoteException;
+    public void haveCommitted(Transaction t) throws RemoteException;
+    public boolean getDecision(Transaction t, Chord p) throws RemoteException;
+}
